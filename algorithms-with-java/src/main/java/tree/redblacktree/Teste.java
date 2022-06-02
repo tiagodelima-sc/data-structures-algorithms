c5aaea9a9699a1640e2e23026e79d532b3384082
Tiago de Lima <tiago.l07@aluno.ifsc.edu.br>
2022-06-01 21:38:02 -0300

    exercise red–black tree

---
+++ algorithms-with-java/src/main/java/tree/redblacktree/Teste.java
@@ -0,0 +1,17 @@
+package tree.redblacktree;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		Arvore arv = new Arvore();
+		arv.adiciona(15);
+		arv.adiciona(30);
+		arv.adiciona(14);
+		arv.adiciona(25);
+		arv.adiciona(17);
+		arv.adiciona(88);
+		arv.adiciona(5);
+		arv.print();
+	}
+
+}
